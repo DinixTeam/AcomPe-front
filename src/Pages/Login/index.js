b5cb28f396deeff3bb018728bee7a031c6f4142c
Elison12 <epassos352@gmail.com>
2022-04-21 11:52:04 -0300

    login-page_styled

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { Container } from "./style";
+import "./index.css";
 
 const Login  = () =>{
     return(
-        <Container>
-            
-
-        </Container>
+        <div class = "form-container">
+            <form class = "register-form" id="formulario">
+                <input class = "campo-cpf"
+                    id="cpf"
+                    // placeholder="Cpf do Paciente"
+                />
+                <input class = "campo-senha"
+                    id="senha"
+                    // placeholder="senha"                
+                />
+                {/* <buttom><span class="bottom-entrar"></span>Entrar</buttom> */}
+                <input class="buttom-entrar" type="submit" value= "ENTRAR"/>
+            </form>
+        </div>
     )
 }
 
--- src/Pages/Login/style.js
+++ src/Pages/Login/style.js
@@ -3,10 +3,19 @@ import { Container } from "./style";
 
 const Login  = () =>{
     return(
-        <Container>
-            
-
-        </Container>
+        <div class = "form-container">
+            <form class = "register-form">
+                <input 
+                    id="cpf"
+                    placeholder="Cpf do Paciente"
+                />
+                <input
+                    id="senha"
+                    placeholder="senha"                
+                />
+            </form>
+        </div>
+        
     )
 }
 
