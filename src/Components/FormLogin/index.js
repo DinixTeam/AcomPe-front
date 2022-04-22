229d82cc9116001ced194d71b5b7a67120a99751
jonny paulino <jonnyzico10@gmail.com>
2022-04-22 12:53:13 -0300

    Login

---
+++ src/Components/FormLogin/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Form, Image, Button, Input } from './style';
+import logo from '../../Assets/logo.png'
+
+import { useHistory } from "react-router-dom";
+
+const FormLogin = () => {
+
+    const history = useHistory();
+
+    const linkCadastro = () => {
+        history.push('/cadastro');
+    }
+    return(
+        <div>
+            <Image src={logo}/>
+            <Form>
+                <h1 style={{marginBottom: '10px'}}>Login</h1>
+                    <Input>
+                        <h2>Nome de usuario </h2>
+                        <input 
+                        />
+                    </Input>
+                    <Input>
+                        <h2>Senha</h2>
+                        <input 
+                        />
+                    </Input>
+                    <div 
+                    style={{fontSize: '15px', marginBottom: '10px', cursor: 'pointer', fontWeight: 'bold'}} 
+                    onClick={linkCadastro}>
+                        Não tenho Cadastro
+                    </div>
+                    <Button>Entrar</Button>
+            </Form>
+        </div>
+    );
+}
+
+export default FormLogin;
---
+++ src/Pages/Cadastro/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Cadastro = () => {
+    return(
+        <div>
+            bsdjb
+        </div>
+
+    )
+}
+
+export default Cadastro;
