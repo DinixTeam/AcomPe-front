0ab4f6e30942f0a96b9178397aaed9b097f23db6
jonnypaulino <jonnyzico10@gmail.com>
2022-05-06 11:35:37 -0300

    tela inicial

---
+++ src/Components/Consulta/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    grid-area: 'CS';
+    background: linear-gradient(0deg, rgba(255, 255, 255, 0.05), rgba(255, 255, 255, 0.05)), #7698DB;
+    height: 100%;
+
+    button{
+        background-color: #d69af0;
+        border: none;
+        width: 180px;
+        height: 40px;
+        border-radius: 20px;
+        color: white;
+        font-size: 1vw;
+        font-weight: bold;
+        cursor: pointer;
+    }
+`;
+
--- src/Pages/TelaInicial/index.js
+++ src/Pages/TelaInicial/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import TopBar from "../../Components/TopBar";
 import MenuClient from "../../Components/MenuClient";
 import { Grid } from "./style";
+import Consulta from "../../Components/Consulta";
 
 const TelaInicial = () => {
     return(
          <Grid>
             <TopBar />
             <MenuClient />
+            <Consulta />
          </Grid>
     )
 } 
